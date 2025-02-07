700ffa3b2fafbcf8f8300356c3a29f715ddeecc1
Ocramaru <marcocassar@mdcusa.net>
2025-02-06 22:57:15 -0600

    Moving the Player
    Added an Input System to handle the keys for us, Added basic Physics based movement script called PlayerController, added some hierarchy folders to keep things organized and thats it.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb; 
+    
+    // Move Vals
+    private float movementX;
+    private float movementY;
+    
+    // Public Values
+    public float speed = 0; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent <Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // Physics Based Movement
+    private void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        _rb.AddForce(movement * speed); 
+    }
+    
+    void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+        movementX = movementVector.x; 
+        movementY = movementVector.y;
+    }
+}
