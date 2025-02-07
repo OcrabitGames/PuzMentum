0e1b29a33a2c4a49619d7e67c7cad96344a641c6
Ocramaru <marcocassar@mdcusa.net>
2025-02-06 23:20:56 -0600

    Detecting Collisions with Collectibles
    Updated PlayerController so that it disables the PickUp object when it collides with it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,4 +37,12 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x; 
         movementY = movementVector.y;
     }
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
